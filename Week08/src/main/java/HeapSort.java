8c4497b57bbc9eb17f2a94fe72997ad510740f52
ruirui.qu <ruirui.qu@qunar.com>
2020-08-03 10:48:33 +0800

    add

--- Week08/src/main/java/HeapSort.java
+++ Week08/src/main/java/HeapSort.java
@@ -4,48 +4,80 @@
 
 public class HeapSort {
 
-    public static void heapify(int[] array, int length, int i) {
+    //完全二叉树
+    //堆，parent> child
 
-        int left = 2 * i + 1;int rigt = 2 * i + 2;
+    public static void heapify(int[] tree, int n, int i) {
 
-        int largest = i;
-
-        if (left < length && array[left] > array[largest]) {
-            largest = left;
-        }
-
-        if (rigt < length && array[rigt] > array[largest]) {
-            largest = rigt;
-        }
-
-        if (largest != i) {
-            int temp = array[i]; array[i] = array[largest];  array[largest] = temp;
-            heapify(array, length, largest);
-        }
-    }
-
-    public static  void headpSort(int[] array){
-        if(array.length==0){
+        //递归终止条件
+        if (i >= n) {
             return;
         }
 
-        int length = array.length;
+        //左孩子
+        int c1 = 2 * i + 1;
+        //右孩子
+        int c2 = 2 * i + 2;
 
-        for (int i = length / 2 - 1; i >= 0; i--) {
-            heapify(array, length, i);
+        int max = i;
+
+        //在父亲节点，左孩子，右孩子节点之间寻找最大值
+        if (c1 < n && tree[c1] > tree[max]) {
+            max = c1;
         }
 
-        for (int i = length - 1; i >= 0; i--) {
-            int temp = array[0]; array[0] = array[i]; array[i] = temp;
-            heapify(array, i, 0);
+        if (c2 < n && tree[c2] > tree[max]) {
+            max = c2;
         }
+
+
+        //如果当前节点不是最大值，则需要进行节点交换
+        if (max != i) {
+            swap(tree, max, i);
+
+            //轮询进行heapyfy
+            heapify(tree, n, max);
+        }
+    }
+
+    private static void buildHeap(int[] tree, int n) {
+        int lastNode = n - 1;
+        int parent = (lastNode - 1) / 2;
+
+        for (int i = parent; i >= 0; i--) {
+            heapify(tree, n, i);
+        }
+    }
+
+    private static void heapSort(int[] tree, int n) {
+        buildHeap(tree, n);
+
+        for (int i = n - 1; i >= 0; i--) {
+            swap(tree, i, 0);
+            heapify(tree, i, 0);
+        }
+    }
+
+    private static void swap(int[] tree, int max, int i) {
+        int temp = tree[max];
+        tree[max] = tree[i];
+        tree[i] = temp;
     }
 
 
     public static void main(String[] args) {
-        int[] array = new int[]{3,10,7,9,2,1,1};
+//        int[] array = new int[]{4, 10, 3, 5, 1, 2};
+//
+//        heapify(array, 6, 0);
 
-        headpSort(array);
-        System.out.println("===>"+array);
+//        int[] tree = new int[]{2,5,3,1,10,4};
+//        buildHeap(tree, 6);
+//        System.out.println("===>" + tree);
+
+        int[] tree = new int[]{2, 5, 3, 1, 10, 4};
+
+        heapSort(tree, 6);
+
+        System.out.println("====>" + tree);
     }
 }
